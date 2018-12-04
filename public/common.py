6a9bd6bf9bda1f7c8eb4e2cd2905a5f97630969d
gkk <gkk163163@163.com>
2018-12-04 14:56:57 +0800

    新增UII项目

--- public/common.py
+++ public/common.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
+
+
+
 
 driver = webdriver.Chrome(executable_path="D:\\Tools\\Pycharm\\UI\\driver\\chromedriver.exe")
 driver.maximize_window()
