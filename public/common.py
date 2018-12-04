ef3ae1c5d21c798654c195853e0b76edd5591021
gkk <gkk163163@163.com>
2018-12-04 16:53:24 +0800

    在百度首页添加搜索测试教程网，进行结果判断

--- public/common.py
+++ public/common.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from time import sleep
 
 
 
@@ -6,3 +7,10 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path="D:\\Tools\\Pycharm\\UI\\driver\\chromedriver.exe")
 driver.maximize_window()
 driver.get("http:www.baidu.com")
+sleep(5)
+driver.find_element_by_xpath('//*[@id="kw"]').send_keys("测试教程网")
+sleep(1)
+driver.find_element_by_xpath('//*[@id="su"]').click()
+print("输入测试教程网，点击搜索按钮")
+assert "测试教程网_百度搜索" == driver.title
+assert "[优惠活动]" in driver.page_source
